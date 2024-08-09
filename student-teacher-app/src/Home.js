5c9de387d1b838ff17f2facc625375311f23d664
javiersteven0612 <javierwork2004@gmail.com>
2024-08-09 14:04:05 +0530

    Update Home.js

--- student-teacher-app/src/Home.js
+++ student-teacher-app/src/Home.js
@@ -1,22 +1,23 @@
-  /* Home.js */
+// Home.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
+import FloatingButton from './FloatingButton'; // Import the FloatingButton component
 
-  import React from 'react';
-  import { Link } from 'react-router-dom';
-  import './Home.css';
-
-  function Home() {
-    return (
-      <div className="home-container">
-        <div className="content">
-          <h1 className="title animated-text">Welcome to Student-Teacher Portal</h1>
-          <div className="button-container">
-            <Link to="/student" className="student-button">Student</Link>
-            <div className="button-gap"></div>
-            <Link to="/teacher" className="teacher-button">Teacher</Link>
-          </div>
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="content">
+        <h1 className="title animated-text">Welcome to Student-Teacher Portal</h1>
+        <div className="button-container">
+          <Link to="/student" className="student-button">Student</Link>
+          <div className="button-gap"></div>
+          <Link to="/teacher" className="teacher-button">Teacher</Link>
         </div>
       </div>
-    );
-  }
+      <FloatingButton /> {/* Add the FloatingButton component */}
+    </div>
+  );
+}
 
-  export default Home;
+export default Home;
